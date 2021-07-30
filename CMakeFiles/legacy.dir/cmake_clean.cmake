d1a0168e2387b299ea96216460386862be02fbd5
xadhrit <maharajrabel@gmail.com>
2021-07-30 13:39:20 -0400

    cmake basic

---
+++ CMakeFiles/legacy.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/legacy.dir/sys/creat.c.o"
+  "CMakeFiles/legacy.dir/sys/lseek.c.o"
+  "CMakeFiles/legacy.dir/sys/open.c.o"
+  "CMakeFiles/legacy.dir/sys/truncate.c.o"
+  "CMakeFiles/legacy.dir/sys/read.c.o"
+  "CMakeFiles/legacy.dir/sys/write.c.o"
+  "CMakeFiles/legacy.dir/memory-allocator/alloc.cc.o"
+  "CMakeFiles/legacy.dir/Protection/main.c.o"
+  "CMakeFiles/legacy.dir/ticketing_sys/main.c.o"
+  "CMakeFiles/legacy.dir/banker_simulation/main.c.o"
+  "CMakeFiles/legacy.dir/monte/main.c.o"
+  "CMakeFiles/legacy.dir/management/main.c.o"
+  "CMakeFiles/legacy.dir/banker_simulation/implementation.c.o"
+  "CMakeFiles/legacy.dir/bank/term.c.o"
+  "CMakeFiles/legacy.dir/tele++/main.cpp.o"
+  "CMakeFiles/legacy.dir/stack/stack.c.o"
+  "legacy.pdb"
+  "legacy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/legacy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
