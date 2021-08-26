7bd2810e012cbf2434ca68e7ade1d70668ebe1fa
xadhrit <maharajrabel@gmail.com>
2021-08-26 11:16:51 -0400

    j

--- run.sh
+++ run.sh
@@ -3,9 +3,16 @@
 CYAN="\e[36m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
+FILE=Makefile
 
 cmd(){
    cmake CMakeLists.txt
+   if [ -f "$FILE" ] ; then
+       make
+   else
+     cmake CMakeLists.txt
+     make
+   fi
 }
 
 cmd
