946de10ae561ad01d272b0cda6e8e86931e9021b
xadhrit <maharajrabel@gmail.com>
2021-08-26 11:17:47 -0400

    point

--- tele++/main.cpp
+++ tele++/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include <graphics.h>
 #include <string.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <stdio.h>
-#include <iomanip.h>
-#include <dos.h>
 
-using std::cout;
-using std::cin;
-using std:endl;
+#define KRED "\x1B[31m"
+#define KG  "\x1B[32m"
+#define KY  "\x1B[33m"
+#define KB  "\x1B[34m"
+#define KM  "\x1B[35m"
+
+FILE *fp;
 
 class tele{
   public:
@@ -21,22 +21,37 @@ class tele{
       void  get(void);
       void show(void);
       void modify(void);
+      void delete_user(void);
+     
 };
 
 void tele::get(void){
-   cout << "\n Enter name : ";
-   cin >> Name;
-   cout << "\n Enter Address : ";
-   cin >> Address;
-   cout << "\n Enter Phone Number : ";
-   cin >> ph_no;
+   std::cout << " Enter name : " << std::endl;
+   std::cin >> Name;
+   std::cout << " Enter Address : " << std::endl ;
+   std::cin >> Address;
+   std::cout << "\n Enter Phone Number : " << std::endl;
+   std::cin >> ph_no;
   
 }
 
 void tele::show(void) {
-   cout << "\n ";
-   cout << "Name :   " << "\t" << Name << endl;
-   cout << "Address : " << "\t" << Address << endl;
-   cout << "Phone Number : " << "\t" << ph_no << endl;
+   std::cout << "\n ";
+   std::cout << "Name :   " << "\t" << Name << std::endl;
+   std::cout << "Address : " << "\t" << Address << std::endl;
+   std::cout << "Phone Number : " << "\t" << ph_no << std::endl;
 
+}
+
+int main(){
+     char ch;
+     system("clear");
+     std::cout << KRED << "Welcome to Phone Book in C++ " << std::endl;
+     std::cout << KM  <<  "1. Add New User  " << std::endl; 
+     std::cout << KG  <<  "2. Show User : " << std::endl;
+     std::cout << KB  <<  "3. Edit User  " << std::endl;
+     std::cout << KM  <<  "4. Delete User  " << std::endl;
+     std::cout << "Choose One Option : ";
+     std::cin >> ch;
+     return 0;
 }
